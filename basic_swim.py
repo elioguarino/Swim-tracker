b729b43bfa3bea9c7fb839eb27962e159d2093cb
Elio Guarino <elioguarino11@outlook.com>
2026-08-14 15:17:47 +0100

    added average pace over week - bugfix 7

--- basic_swim.py
+++ basic_swim.py
@@ -658,12 +658,12 @@ def render_swim_section():
             #ok now we have total distance swam (with times added on - otherwise it doesnt count) in the week, and total time taken in the week
 
             if total_timed_week_meters > 0:
-                raw_pace_seconds = (total_week_seconds/total_timed_week_meters)
+                raw_pace_seconds = (total_week_seconds/total_timed_week_meters)*100
                 pace_minutes, pace_seconds = divmod(int(raw_pace_seconds), 60)
 
                 st.markdown(
                     f"<div style='text-align:center; font-weight:bold; font-size:1.0rem; color:#06304a;'>"
-                    f"Average pace (last 7 days): {pace_minutes:,},{pace_seconds:,} per 100m </div>",
+                    f"Average pace (last 7 days): {pace_minutes:,}:{pace_seconds:,} per 100m </div>",
                     unsafe_allow_html=True
                 )   
 
